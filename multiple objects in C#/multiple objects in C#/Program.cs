8b64087fa883e656a4efec8adf1206140f7aeeb1
F m Bushry <121561467+Bushryfm@users.noreply.github.com>
2024-08-15 23:21:58 +0530

    Add files via upload

---
+++ multiple objects in C#/multiple objects in C#/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using multiple_objects_in_C_;
+
+calculation obj = new calculation();
+calculation obj1 = new calculation();
+int total = obj.addition(12, 14);
+int total1 = obj1.sub(12, 14);
+
+Console.WriteLine(total);
+Console.WriteLine(total1);
+
---
+++ multiple objects in C#/multiple objects in C#/calculation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiple_objects_in_C_
+{
+    internal class calculation
+    {
+        internal int addition(int a, int b)
+        {
+            return a + b;
+        }
+        internal int sub(int a, int b)
+        {
+
+            return a - b;
+        }
+    }
+}
